54fc1666bd1ab1846192e9cd8eb4c404642fd60b
James Burnett <james.c.burnett@icloud.com>
2024-11-07 08:52:37 -0500

    added code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/IntakeConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/IntakeConstants.java
@@ -6,4 +6,7 @@ public interface IntakeConstants {
     double INTAKE_D = 0;
     double INTAKE_FF = 0;
     double SLIDE_SPEED = 0.3;
+    double INTAKE_SPEED = 0.5;
+    double INTAKE_DELTA_LENGTH = 1;
+    double INTAKE_DELTA_THETA = 0.5;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 import org.firstinspires.ftc.teamcode.constants.IntakeConstants;
@@ -22,20 +23,37 @@ public class Intake implements IntakeConstants {
     boolean isBlueAlliance;
 
     public Intake(HardwareMap hwMap,  boolean isBlue) {
-
-
         slide = hwMap.get(DcMotorEx.class, "intakeSlide");
         slide.setDirection(DcMotorEx.Direction.FORWARD);
         slide.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         slide.setVelocityPIDFCoefficients(INTAKE_P, INTAKE_I, INTAKE_D, INTAKE_FF);
 
+        intakeOne = hwMap.get(CRServoImplEx.class, "intakeOne");
+        intakeTwo = hwMap.get(CRServoImplEx.class, "intakeTwo");
+
+        intakeOne.setDirection(DcMotorSimple.Direction.FORWARD);
+        intakeTwo.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        wristOne = hwMap.get(ServoImplEx.class, "wristOne");
+        wristTwo = hwMap.get(ServoImplEx.class, "wristTwo");
+
+        wristOne.setDirection(Servo.Direction.FORWARD);
+        wristTwo.setDirection(Servo.Direction.REVERSE);
 
         isBlueAlliance = isBlue;
-
     }
 
     public void slide(double power){
         slide.setPower(power * SLIDE_SPEED);
+    }
+
+    public void runIntake(double power){
+        intakeOne.setPower(power * INTAKE_SPEED);
+        intakeTwo.setPower(power * INTAKE_SPEED);
+    }
+
+    public void wristToPos(double pos){
+        
     }
 
 }
